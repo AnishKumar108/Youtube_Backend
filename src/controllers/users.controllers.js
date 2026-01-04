40be8a1a62112e1e647f86848c7c06b98563a8dc
Anish Kuamr <kumaranishabd07@gmail.com>
2026-01-04 15:07:53 +0530

    Final setup done

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,10 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler((req,res) => {
+    res.status(200).json({message:"OK"})
+})
+// const registerUser = (req, res) => {
+//     res.status(200).json({ message: "User registered successfully" });
+// }
+
+export default registerUser
